15ad3dc43c47afa5ff2933e7a87bedd80a7deaab
Math_th06 <mathmarqper0624@gmail.com>
2025-07-10 22:04:56 -0300

    Atualização de comentarios e print para o terminal

--- database/banco.py
+++ database/banco.py
@@ -1,18 +1,21 @@
 from pathlib import Path
 import json
 
+#Caminhos e criação de arquivos e diretorios
 caminho = Path(__file__).parent.parent
 reposi = caminho/"data"
 arquivo = reposi/"cliente.json"
 
 reposi.mkdir(exist_ok=True)
 
-
+#função para coletar os dados e transformar em JSON
 def salvar_arquivo(cliente_dict):
+    #Condição caso o arquivo não seja existente sua criação
     if not arquivo.exists():
         with open(arquivo, "w", encoding="utf-8") as arq:
             json.dump([], arq, indent=4)
-        
+
+    # leitiura e escrita de novos dados    
     with open(arquivo, "r+", encoding="utf-8") as arq:
         dados = json.load(arq)
         dados.append(cliente_dict)
--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from model.cadastro import Cliente
 from database.banco import salvar_arquivo
 
+# dados do cliente
+print("-----------------")
+print()
+print("Dados cliente")
 
 nome = input("Nome: ")
 cpf = input("CPF: ")
 
+print()
+print("-----------------")
+
+#chamando classe que amarzena os valores do cliente
 usuario = Cliente(nome, cpf)
 
+#Salvamento dos dados no arquivo
 salvar_arquivo(usuario.to_dict())
 
